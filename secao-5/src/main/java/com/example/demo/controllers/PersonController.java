1fd9ec6a4b8814c3c5cc3b2f8b0a3b5a2d5ad099
Ronald Marques <ronaldpmarques01@gmail.com>
2025-04-23 18:41:32 -0300

    Completion of section 6 of the course

---
+++ secao-5/src/main/java/com/example/demo/controllers/PersonController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controllers;
+
+import com.example.demo.PersonServices;
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @RequestMapping("/{id}")
+    public Person findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @RequestMapping("/all")
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(
+            value = "/{id}",
+            method = RequestMethod.DELETE
+    )
+    public void delete(@PathVariable("id") Long id) {
+        service.delete(id);
+    }
+}
--- secao-5/src/main/java/com/example/demo/model/Greeting.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.model;
-
-public record Greeting(long id, String name) { }
