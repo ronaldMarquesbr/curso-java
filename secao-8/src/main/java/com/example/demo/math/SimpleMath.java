ace2d7d7f1a13deca36993a6d48711f6e14e50d4
Ronald Marques <ronaldpmarques01@gmail.com>
2025-05-01 08:30:18 -0300

    Add section 11 project

---
+++ secao-8/src/main/java/com/example/demo/math/SimpleMath.java
@@ -0,0 +1,12 @@
+package com.example.demo.math;
+
+public class SimpleMath {
+
+    public Double sum(Double numberOne, Double numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public Double subtraction(Double numberOne, Double numberTwo) {
+        return numberOne - numberTwo;
+    }
+}
